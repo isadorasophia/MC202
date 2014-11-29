b86c43b19a4f0f25bb0e1a83c29c6219957a50c8
isadorasophia <isaecia@gmail.com>
2014-11-29 21:12:51 -0200

    Now its fine YAY! :)

--- Laboratório 08/main.c
+++ Laboratório 08/main.c
@@ -1,39 +1,48 @@
+/* Isadora Sophia Garcia Rodopoulos
+ * RA: 158018
+ * MC202B - LAB08 - Menor Ancestral Comum */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arvores.h"
+#include "arvore.h"
 
-#define MAX_LENGHT 4
+#define MAX_LENGHT 5
 
 int main () {
-    char[MAX_LENGHT] operation;
+    char operation[MAX_LENGHT];
     int N, M, a, b;
-    Node *florest;
+    Tree *florest;
     
     scanf("%d %d", &N, &M);
     
-    /* Create N trees linked to a single vertex */
-    florest = initializeTrees(N);
+    /* Create N trees linked to a single vertex and set them right */
+    florest = malloc ((N + 1) * sizeof(Tree));
+    setFlorest (florest, N + 1);
     
-    while (N--) {
+    /* Read and execute the requested operation */
+    while (M--) {
         scanf("%s", operation);
         
         if (strcmp(operation, "lca") == 0) {
             scanf("%d %d", &a, &b);
             
-            printf("%d\n", lca(a, b));
+            printf("%d\n", lca(a, b, florest));
         }
         else if (strcmp(operation, "link") == 0) {
-            scanf("%d %d", &a, &b)
+            scanf("%d %d", &a, &b);
             
-            florest = link (a, b);
+            link (a, b, florest, N + 1);
         }
         else if (strcmp(operation, "cut") == 0) {
-            scanf("%d", &a)
+            scanf("%d", &a);
             
-            florest = cut (a);
+            cut (a, florest, N + 1);
         }
     }
+    
+    /* Deallocate memory */
+    free (florest);
     
     return 0;
 }
