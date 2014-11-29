d889616b155884c633f4068b134f38f589249a29
isadorasophia <isaecia@gmail.com>
2014-11-28 23:27:34 -0200

    OH god...

---
+++ Laboratório 08/Mode 2/arvore.h
@@ -0,0 +1,23 @@
+/* Library responsible for implementing functions related to florest of trees */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Basic structure of a tree */
+typedef struct Tree {
+    int parent;
+    int height;
+} Tree;
+
+/* Return N trees linked to a single vertex */
+void setFlorest (Tree *florest, int size);
+
+/* Link tree A to tree B */
+void link (int A, int B, Tree *florest, int size);
+
+/* Cut the link between A and its parent */
+void cut (int A, Tree *florest, int size);
+
+/* Find the lowest common ancestor between A and B */
+int lca (int A, int B, Tree *florest, int max);
--- Laboratório 08/main.c
+++ Laboratório 08/main.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "arvores.h"
 
 #define MAX_LENGHT 4
 
 int main () {
-    char[MAX_LENGHT] op;
-    int N, M;
+    char[MAX_LENGHT] operation;
+    int N, M, a, b;
     Node *florest;
     
     scanf("%d %d", &N, &M);
     
     /* Create N trees linked to a single vertex */
-    florest = createTrees(N);
+    florest = initializeTrees(N);
     
     while (N--) {
-        scanf("%s", op);
+        scanf("%s", operation);
+        
+        if (strcmp(operation, "lca") == 0) {
+            scanf("%d %d", &a, &b);
+            
+            printf("%d\n", lca(a, b));
+        }
+        else if (strcmp(operation, "link") == 0) {
+            scanf("%d %d", &a, &b)
+            
+            florest = link (a, b);
+        }
+        else if (strcmp(operation, "cut") == 0) {
+            scanf("%d", &a)
+            
+            florest = cut (a);
+        }
     }
     
     return 0;
